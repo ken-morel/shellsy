f489d1a941a6a78e95798c6bebc864059b4d3c57
Restyled.io <commits@restyled.io>
2024-08-17 12:45:03 +0000

    Restyled by black

--- src/tests/test_preview.py
+++ src/tests/test_preview.py
@@ -4,7 +4,7 @@ from shellsy.interpreter import S_Interpreter as Interp
 def test_preview():
     inter = Interp()
     assert inter.eval("echo 3") == 3
-    assert inter.eval("echo '3'") == '3'
+    assert inter.eval("echo '3'") == "3"
     inter.eval("echo /3/")
     assert inter.eval("echo 3") == 3
     inter.eval("echo 3.3")
