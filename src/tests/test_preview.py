6444424bb986bb56853a4f9abc00a840d915285a
ken-morel <engonken8@gmail.com>
2024-08-17 14:03:23 +0100

    Corrected tests (#29)
    * Update and rename test_parse_args.py to test_preview.py
    
    * Correctes shellsy.Shellsy.écho command
    
    * Create restyled.yml

---
+++ src/tests/test_preview.py
@@ -0,0 +1,10 @@
+from shellsy.interpreter import S_Interpreter as Interp
+
+
+def test_preview():
+    inter = Interp()
+    assert inter.eval("echo 3") == 3
+    assert inter.eval("echo '3'") == '3'
+    inter.eval("echo /3/")
+    assert inter.eval("echo 3") == 3
+    inter.eval("echo 3.3")
